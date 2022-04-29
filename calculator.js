d33fa913150bc98cec3e4b4efb788d92a95ac2a5
Imran Price <imranprice96@gmail.com>
2022-04-30 00:47:27 +1200

    Setup boilerplate, Add basic math functions

---
+++ calculator.js
@@ -0,0 +1,11 @@
+function add(a,b){return a+b};
+
+function subtract(a,b){return a-b}
+
+function multiply(a,b){return a*b}
+
+function divide(a,b){return a/b}
+
+function operate(operator, a, b){
+    return operator(a,b);
+}
