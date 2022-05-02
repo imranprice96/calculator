ef9b665dbd6249b62399915e0b182f9e6ff2f32f
Imran Price <imranprice96@gmail.com>
2022-05-03 01:12:03 +1200

    Add data keys for buttons and test methods
    Add function boilerplates
    Add query selectors for all the buttons
    Remove test listeners

--- app.js
+++ app.js
@@ -1,23 +1,70 @@
+//let currentOperation = '';
+//let firstNumber, secondNumber = '';
+
+//TODO
+//Add way to keep track of the current selection
+//Maybe store everything in one string the use split or slice
+//or append everything into one string then slice
 
 const screen = document.getElementById('screen');
 
-const buttons = document.querySelectorAll('.buttons');
-buttons.forEach(btn => btn.addEventListener('click', (e) => {
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const backspaceButton = document.querySelector('[data-backspace]');
+const clearButton = document.querySelector('[data-clear]');
+
+console.log(numberButtons);
+numberButtons.forEach(btn => btn.addEventListener('click', (e) => {
     if(isButton(e)) {
-        console.log(e.target.value);
-        screen.textContent += e.target.value;
+        //THIS CAN BE SIMPLIFIED TO IF !CURRENTOPERATION -> FIRST NUMBER
+        //ELSE SECOND NUMBER
+        /*if(!firstNumber){
+            firstNumber = e.target.value.toString();
+            updateDisplay(e);
+        }else if(firstNumber && !currentOperation) {
+            firstNumber += e.target.value.toString();
+            updateDisplay(e);
+        }else if(firstNumber && currentOperation) {
+            secondNumber += e.target.value.toString();
+            updateDisplay(e);
+        };*/
     }
 }));
 
-const clearButtons = document.querySelectorAll('.clear-container');
-clearButtons.forEach(btn => btn.addEventListener('click', (e) => {
-    if(e.target.value === 'clear'){screen.textContent = ''; return};
+operationButtons.forEach(btn => btn.addEventListener('click', (e) => {
     if(isButton(e)) {
-        console.log(e.target.value);
-        screen.textContent += e.target.value;
-    } else {return}
+        chooseOperation(e.target.value);
+        updateDisplay(e);
+    }
 }));
 
 function isButton(e){
     return e.target.nodeName === 'BUTTON';
 }
+
+function clearScreen(){
+    currentOperation, firstNumber, secondNumber = '';
+};
+
+function backspace(){
+
+};
+
+function appendNumber(){
+
+};
+
+function chooseOperation(operator){
+    /*if(!currentOperation && firstNumber){
+        currentOperation = operator;
+    }*/
+};
+
+function compute(){
+
+};
+
+function updateDisplay(e){
+    screen.textContent += e.target.value;
+};
